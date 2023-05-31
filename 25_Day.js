04d2901b384b943731fc9a6da26b434769720abc
Chandra Sai Jagadeesh M <85401649+chandumj667@users.noreply.github.com>
2023-05-31 11:00:04 +0530

    Day 24, 25, 26, 27 Code Uploaded

---
+++ 25_Day.js
@@ -0,0 +1,56 @@
+/**
+ * 2618. Check if Object Instance of Class
+ * LeetCode Problem Link: https://leetcode.com/problems/check-if-object-instance-of-class/?utm_campaign=PostD25&utm_medium=Post&utm_source=Post&gio_link_id=qPkbxBwR
+ * 
+ * Write a function that checks if a given value is an instance of a given class or superclass. For this problem, an object is considered an instance of a given class if that object has access to that class's methods.
+
+There are no constraints on the data types that can be passed to the function. For example, the value or the class could be undefined.
+
+
+Example 1:
+
+Input: func = () => checkIfInstanceOf(new Date(), Date)
+Output: true
+Explanation: The object returned by the Date constructor is, by definition, an instance of Date.
+
+
+Example 2:
+
+Input: func = () => { class Animal {}; class Dog extends Animal {}; return checkIfInstanceOf(new Dog(), Animal); }
+Output: true
+Explanation:
+class Animal {};
+class Dog extends Animal {};
+checkIfInstance(new Dog(), Animal); // true
+
+Dog is a subclass of Animal. Therefore, a Dog object is an instance of both Dog and Animal.
+
+
+Example 3:
+
+Input: func = () => checkIfInstanceOf(Date, Date)
+Output: false
+Explanation: A date constructor cannot logically be an instance of itself.
+Example 4:
+
+Input: func = () => checkIfInstanceOf(5, Number)
+Output: true
+Explanation: 5 is a Number. Note that the "instanceof" keyword would return false. However, it is still considered an instance of Number because it accesses the Number methods. For example "toFixed()".
+ */
+
+// JS Code
+
+/**
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj === null || obj === undefined || typeof classFunction !== 'function')
+        return false;
+    return Object(obj) instanceof classFunction;
+};
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
